5d9d2ae4c261c01b94203b0371edfac643511895
MickMelon <michael.mcmillan@outlook.com>
2019-12-11 22:27:41 +0000

    Client validation IP

--- web/app/Config.php
+++ web/app/Config.php
@@ -6,4 +6,11 @@ class Config {
     const DB_NAME = 'miniproject';
     const DB_USER = 'root';
     const DB_PASS = 'P@ssw0rd';
+
+    const ALLOWED_HOSTS = array(
+        '127.0.0.1',
+        'localhost',
+        '89.238.154.167', // My IP
+        '35.174.12.122' // AWS IP
+    );
 }
--- web/app/Controllers/ApiController.php
+++ web/app/Controllers/ApiController.php
@@ -3,6 +3,7 @@ namespace App\Controllers;
 
 use App\Models\SensorModel;
 use App\Json;
+use App\Config;
 
 class ApiController {
     private $sensorModel;
@@ -12,6 +13,8 @@ class ApiController {
     }
 
     public function latest() {
+        if (!$this->validateClient()) return;
+
         $latest = $this->sensorModel->latest();
 
         $json = new Json($latest, true);
@@ -19,6 +22,8 @@ class ApiController {
     }
 
     public function get() {
+        if (!$this->validateClient()) return;
+
         if (isset($_GET['time']) && (strtolower($_GET['time']) != 'all')) {
             $data = $this->sensorModel->time($_GET['time'], 1);
         } else {
@@ -35,11 +40,17 @@ class ApiController {
      * @return void
      */
     public function create() {
+        if (!$this->validateClient()) return;
+
         $jsonbody = file_get_contents('php://input');
         $json = json_decode($jsonbody, false);
 
         $this->sensorModel->save($json->temperature, $json->humidity, $json->system_temp);
 
         $showJson = new Json(['message' => "Received data: [temp:$json->temperature|hum:$json->humidity|systmp:$json->system_temp]"]);
+    }
+
+    private function validateClient() {
+        return (in_array($_SERVER['REMOTE_ADDR'], Config::ALLOWED_HOSTS));
     }
 }
